0ce4f9b698c683621f89571fede2a5128c405667
Sairesse Victorino <71283657+sairessy@users.noreply.github.com>
2020-09-18 09:24:40 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,22 +6,12 @@ $(document).ready(()=> {
 
 
     // DEAL WITH THE MAP
-//     var mymap = L.map('mapid').setView([51.505, -0.09], 2);
 
     // Init the map
-
-//     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//         maxZoom: 18,
-//         id: 'mapbox/streets-v11',
-//         tileSize: 512,
-//         zoomOffset: -1,
-//         accessToken: 'your.mapbox.access.token'
-//     }).addTo(mymap);
     
     var mymap = L.map('mapid', {
     'center': [51.505, -0.09],
-    'zoom': 2,
+    'zoom': 4,
     'layers': [
             L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 'attribution': 'Map data &copy; OpenStreetMap contributors'
